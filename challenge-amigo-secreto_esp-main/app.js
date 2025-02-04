dac2c7fc7422427038fd1332a0d20f5721971286
David Estrada <david-a.m.p.9@hotmail.com>
2025-02-03 20:30:59 -0500

    pull: Subida archivos

---
+++ challenge-amigo-secreto_esp-main/app.js
@@ -0,0 +1,31 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+const inputAmigos = document.querySelector(".input-name");
+const listaAmigos = document.querySelector(".name-list");
+const resultado = document.querySelector(".result-list");
+
+let arrayAmigos = [];
+
+let numberRandom = Math.floor(Math.random()*arrayAmigos);
+
+
+
+
+const agregarAmigo = () =>{
+    if()
+    console.log(inputAmigos.value);
+    arrayAmigos.push(inputAmigos.value);
+    console.log(arrayAmigos);
+    inputAmigos.value = ``;
+    const mostrarAmigos = () =>{
+        listaAmigos.innerHTML = ``;
+        listaAmigos.innerHTML = arrayAmigos;
+    }
+    mostrarAmigos();
+}
+
+
+const sortearAmigo = () =>{
+    resultado.innerHTML = `El amigo sorteado es : ${arrayAmigos[numberRandom]}`;
+}
+
